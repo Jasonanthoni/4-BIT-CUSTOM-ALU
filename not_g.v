9fe2be07881322e1682aa252eeec3fcd3043363b
RahulAK122005 <146237608+RahulAK122005@users.noreply.github.com>
2025-11-03 19:34:23 +0530

    Update not_g.v

--- not_g.v
+++ not_g.v
@@ -1,8 +1,6 @@
 module not_g(
-input [3:0] A,
-output [7:0] Y
+    input [3:0] A,       // input A
+    output [7:0] Y       // output Y
 );
-    assign Y = {4'b000, ~A};
+    assign Y = {4'b0000, ~A};   // perform bitwise NOT and extend to 8 bits
 endmodule
-
-
