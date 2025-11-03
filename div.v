ae3a69918c22b70cbb16d87b455e53cd8f71d8db
JASON ANTHONI <105900282+Jasonanthoni@users.noreply.github.com>
2025-11-03 15:54:33 +0530

    Update div.v

--- div.v
+++ div.v
@@ -1,16 +1,19 @@
 module div (
     input  [3:0] A, B,      
-    output reg [3:0] QUOTIENT,
-    output reg [3:0] REMAINDER
+    output reg [7:0] QUO 
 );
+    reg [3:0] quotient;
+    reg [3:0] remainder;
+
     always @(*) begin
-        if (B != 0) begin
-            QUOTIENT  = A / B;
-            REMAINDER = A % B;
+        if (B != 4'b0000) begin
+            quotient  = A / B; 
+            remainder = A % B;
         end else begin
-            QUOTIENT  = 4'b0000;
-            REMAINDER = 4'b0000; 
+            quotient  = 4'b0000;
+            remainder = 4'b0000; 
         end
+        QUO = {remainder, quotient}; 
     end
 
 endmodule
